f510d4faef3fad8a5bd526a5f08d0a8c9665c9e3
Jac0bAnderson <jake912235@gmail.com>
2016-03-09 15:04:34 -0700

    Changed the notes in the robotSing();

--- src/bot/model/EV3Bot.java
+++ src/bot/model/EV3Bot.java
@@ -62,12 +62,8 @@ public class EV3Bot
 	}
 public void driveRoom()
 {
-	//rotateLeft()
-	//rotateRight()
 	ultrasonicSamples = new float [distanceSensor.sampleSize()];
 	distanceSensor.fetchSample(ultrasonicSamples, 0);
-	//short side drive
-	//shortDrive();
 	robotSing();
 	//if(ultrasonicSamples[0] < 30) 
 	//{
@@ -83,37 +79,22 @@ public void driveRoom()
 	//}
 	
 }
-/**
- * private static String[] notes = { "C3", "C#3", "Db3", "D3", "D#3", "Eb3",
-        "E3", "F3", "F#3", "Gb3", "G3", "G#3", "Ab3", "A3", "A#3", "Bb3",
-        "B3", "C4", "C#4", "Db4", "D4", "D#4", "Eb4", "E4", "F4", "F#4",
-        "Gb4", "G4", "G#4", "Ab4", "A4", "A#4", "Bb4", "B4", "C5", "C#5",
-        "Db5", "D5", "D#5", "Eb5", "E5", "F5", "F#5", "Gb5", "G5", "G#5",
-        "Ab5", "A5", "A#5", "Bb5", "B5", "C6" };
-  private static float[] frequency = { 130.81f, 138.59f, 138.59f, 146.83f,
-        155.56f, 155.56f, 164.81f, 174.61f, 185.0f, 185.0f, 196.0f,
-        207.65f, 207.65f, 220.0f, 233.08f, 233.08f, 246.94f, 261.63f,
-        277.18f, 277.18f, 293.66f, 311.13f, 311.13f, 329.63f, 349.23f,
-        369.99f, 369.99f, 392.0f, 415.3f, 415.3f, 440.0f, 466.16f, 466.16f,
-        493.88f, 523.25f, 554.37f, 554.37f, 587.33f, 622.25f, 622.25f,
-        659.26f, 698.46f, 739.99f, 739.99f, 783.99f, 830.61f, 830.61f,
-        880.0f, 932.33f, 932.33f, 987.77f, 1046.5f };
- */
-	
 
 public void robotSing()
 {
-	Sound.playTone(739, 1000);//G
-	Sound.playTone(698, 1000);//F
-	Sound.playTone(698, 1000);//F
-	Sound.playTone(659,1000);//E
-	Sound.playTone(622,1000);//D#
-	Sound.playTone(587,1000);//D
-	Sound.playTone(466, 500);//A#
-	Sound.playTone(466, 500);//A#
-	Sound.playTone(523, 1000);//C
-	Sound.playTone(261,1000);//C
+	
+	Sound.playTone(1567, 1000);//G
+	Sound.playTone(1396.91, 1000);//F
+	Sound.playTone(1396, 1000);//F
+	Sound.playTone(1318.51,1000);//E
+	Sound.playTone(1244.51,1000);//D#
+	Sound.playTone(1174.66,1000);//D
+	Sound.playTone(932.33, 500);//A#
+	Sound.playTone(932.33, 500);//A#
+	Sound.playTone(1046.50, 1000);//C
+	Sound.playTone(523.25,1000);//C
 }
+
 private void shortDrive()
 {
 	LCD.drawString("Short", xPosition, yPosition);
